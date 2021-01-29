11e27d99405494f57e34f05a20fc69674c84dae3
Mano Vikram <manovik18@gmail.com>
2021-01-29 18:22:06 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter SQLite MVP Architecture",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: Learning(),
+    );
+  }
+}
+
+class Learning extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+void main() => runApp(MyApp());
